76b23668824d94810fef7607adb9d8def1910276
clement.wu <1928856034@qq.com>
2017-01-10 09:56:37 +0800

    1.实现背景图的点击事件，点击放大，再次点击缩小

--- app/src/main/java/com/example/gankdemo/module/home/MainFragment.java
+++ app/src/main/java/com/example/gankdemo/module/home/MainFragment.java
@@ -1,8 +1,12 @@
 package com.example.gankdemo.module.home;
 
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -25,6 +29,8 @@ import butterknife.BindView;
  * MainFragment
  */
 public class MainFragment extends BaseFragment {
+    @BindView(R.id.rootView)
+    CoordinatorLayout rootView;
     @BindView(R.id.viewpager)
     ViewPager viewPager;
     @BindView(R.id.tabLayout)
@@ -32,6 +38,9 @@ public class MainFragment extends BaseFragment {
     @BindView(R.id.iv_icon)
     ImageView iv_icon;
     private List<String> titles = new ArrayList<>();
+    //记录ImageView原始的高度，和放大后的高度
+    private int originalHeight,enLargeHeight;
+    private boolean isEnLarge = false ;
     @Override
     public int onBindLayoutID() {
         return R.layout.fragment_main;
@@ -55,13 +64,58 @@ public class MainFragment extends BaseFragment {
         iv_icon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //获取屏幕的高度
-                int screenHeight = getActivity().getWindowManager().getDefaultDisplay().getHeight();
-                ViewGroup.LayoutParams params = iv_icon.getLayoutParams();
-                params.height = screenHeight;
-                iv_icon.setLayoutParams(params);
+                if(originalHeight==0 && enLargeHeight==0){
+                    //获取屏幕的高度
+//                    enLargeHeight = getActivity().getWindowManager().getDefaultDisplay().getHeight();
+//                    int tabLayoutHeight = tabLayout.getHeight();
+//                    enLargeHeight = enLargeHeight-tabLayoutHeight;
+                    enLargeHeight = rootView.getHeight();
+                    originalHeight = iv_icon.getHeight();
+                }
+                if(isEnLarge){
+                    //执行缩小
+                    zoomImageView(iv_icon,enLargeHeight,1,(float) originalHeight/enLargeHeight,500);
+                    isEnLarge = false;
+                }
+                else{
+                    //执行放大
+                    zoomImageView(iv_icon,originalHeight,1,(float) enLargeHeight/originalHeight,500);
+                    isEnLarge = true;
+                }
             }
         });
+    }
+
+    /**放大控件动画，属性动画
+     * @param imageView
+     * @param beforeHeight
+     * @param fromScale
+     * @param toScale
+     * @param duration
+     */
+    private void zoomImageView(final ImageView imageView, final int beforeHeight, float fromScale, float toScale, long duration){
+        ObjectAnimator anim = ObjectAnimator.ofFloat(imageView,"image",fromScale,toScale).setDuration(duration);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float val = (Float)valueAnimator.getAnimatedValue();
+                updateParams(imageView,beforeHeight,val);
+            }
+        });
+        anim.start();
+    }
+
+    /**更新控件的属性
+     * @param imageView
+     * @param height
+     * @param val
+     */
+    private void updateParams(ImageView imageView,int height,float val){
+        ViewGroup.LayoutParams params = imageView.getLayoutParams();
+        float tempHeight = val*height;
+        params.height = (int)tempHeight;
+        Log.d("Height,val",tempHeight+" , "+val);
+        iv_icon.setLayoutParams(params);
     }
     private void initTabLayout(){
         tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
