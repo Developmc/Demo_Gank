d26db0a95d0163bb274fb19857345b58f652d8ea
developmc <1928856034@qq.com>
2017-02-08 22:53:59 +0800

    将setting页面转为mvp

---
+++ app/src/main/java/com/example/gankdemo/base/mvp/BaseView.java
@@ -0,0 +1,11 @@
+package com.example.gankdemo.base.mvp;
+
+import android.content.Context;
+
+/**BaseView
+ * Created by developmc on 17/2/8.
+ */
+
+public interface BaseView {
+    Context getContext();
+}
